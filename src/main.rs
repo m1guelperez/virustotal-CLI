2b32adf77b3d31441bb40060f75d1f80d4ce3812
FutureChanger <migi.perez@web.de>
2022-01-02 14:24:13 +0100

    Added functionality to either use CLI commands or std::io input.

--- src/main.rs
+++ src/main.rs
@@ -10,28 +10,21 @@ mod response_controller;
 mod request_controller;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let path = Path::new("configfile.txt");
-    let api_key = get_api_key_from_configfile(path);
-
-    let mut urls_without_cli = String::new();
-
-    io::stdin().read_line(&mut urls_without_cli).expect("Failed to read line.");
-
-    if args.len() == 1 && urls_without_cli.is_empty() {
-        eprintln!("You should enter a [URLs]");
-        std::process::exit(1);
-    }
+    let args = process_user_input();
 
     let mut urls: Vec<String> = Vec::new();
-    for arg in env::args().skip(1) {
-        urls.push(arg);
+    for arg in args.iter().skip(1) {
+        urls.push(arg.to_string());
     }
+
+
+    let path = Path::new("configfile.txt");
+    let api_key = get_api_key_from_configfile(path);
 
     let client = RequestControllerClient::new(api_key.as_str());
     if urls.is_empty() {
         std::process::exit(1);
-    }else {
+    } else {
         for url in urls {
             let res = client.send_url_scan(&url);
             let mut all_scan_results = HashMap::new();
@@ -69,4 +62,29 @@ fn translate_path(path: &String) {
     if cfg!(target_os ="Windows") {} else {
         println!("ubuntu")
     }
+}
+
+fn process_user_input() -> Vec<String> {
+    let args: Vec<String> = env::args().collect();
+
+    let mut urls_without_cli = String::new();
+
+    //TODO: Add linux example.
+    if args.len() <= 1 {
+        io::stdin().read_line(&mut urls_without_cli).expect("Failed to read line.");
+    } else {
+        return args;
+    }
+
+    let mut urls_as_vec: Vec<String> = Vec::new();
+    if urls_without_cli.is_empty() {
+        eprintln!("Either provide commands via CLI like that:\n\
+         Windows: .\\virustotal_folderscanner.exe [URLs]\nor enter them directly into the window when
+         starting the .exe");
+        std::process::exit(1);
+    } else {
+        urls_as_vec = urls_without_cli.split(' ').map(|s| s.to_owned()).collect();
+    }
+
+    urls_as_vec
 }
